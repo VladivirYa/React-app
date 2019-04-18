be1d9668725e03a0b46845dc2a9436adb40c2e8c
v.yaroshenko <v.yaroshenko@devcaz.com>
2019-04-18 18:30:08 +0300

    add under component User_info

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import s from "./Profile.module.scss";
+import User_info from "./User_inform/User_info";
+
 
 const Profile = () => {
   return (
@@ -7,23 +9,7 @@ const Profile = () => {
       <div>
         <div className={s.img} />
       </div>
-      <div className={s.wrap}>
-        <div>
-          <img
-            src="https://marketplace.canva.com/MAA_Ae2e8_I/1/0/thumbnail_large/canva-handmade-logo-profile-picture-MAA_Ae2e8_I.jpg"
-            alt=""
-          />
-        </div>
-        <div className={s.description}>
-          <h2>Tony K.</h2>
-          <ul>
-            <li><span>Data of Birth:</span> 2 january</li>
-            <li><span>City:</span> Minsk</li>
-            <li><span>Eduvation:</span> BSU'11</li>
-            <li><span>Web Site:</span> //google.com.ua</li>
-          </ul>
-        </div>
-      </div>
+      <User_info/>
       <div className={s.post}>
         My posts
         <div>New post</div>
