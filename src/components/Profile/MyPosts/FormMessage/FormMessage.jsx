83cb5198eab3313adbf94263747d13c239641683
v.yaroshenko <v.yaroshenko@devcaz.com>
2019-04-19 15:40:48 +0300

    add under component FormMessage

---
+++ src/components/Profile/MyPosts/FormMessage/FormMessage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import s from "./FormMessage.module.scss";
+const FormMessage = () => {
+  return (
+    <div>
+      <div className={s.title}>
+        <h3>My posts</h3>
+      </div>
+      <div className={s.post__wrap}>
+        <div className={s.fild}>
+          <textarea />
+        </div>
+        <div className={s.button}>
+          <button>Add post</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FormMessage;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import s from "./MyPosts.module.scss";
 import Post from "./Post/Post";
+import FormMessage from "./FormMessage/FormMessage";
 
 const MyPosts = () => {
   return (
     <div className={s.post}>
-      <h3>My posts</h3>
-      <div className={s.post__wrap}>
-        <div className={s.fild}>
-          <textarea />
-        </div>
-        <div className={s.button}>
-          <button>Add post</button>
-        </div>
-      </div>
+      <FormMessage/>
       <div className={s.posts}>
         <Post mess="Hello world" lk="15" />
         <Post mess="Hello world" lk="26" />
