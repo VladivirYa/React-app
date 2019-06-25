155d71a096e6dbeba32c06b938fdbd563c99ed62
v.yaroshenko <v.yaroshenko@devcaz.com>
2019-06-25 18:37:25 +0300

    add class in Users.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,6 +18,9 @@ const Navbar = (props) => {
 						<NavLink to="/messages" activeClassName={s.activeLink}>Messages</NavLink>
 					</li>
 					<li className={s.item}>
+						<NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+					</li>
+					<li className={s.item}>
 						<NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
 					</li>
 					<li className={s.item}>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,46 +3,21 @@ import styles from './users.module.scss';
 import * as axios from "axios";
 import userPhoto from '../../images/user.png'
 
-let Users = (props) => {
+class Users extends React.Component {
 
-	if (props.users.length === 0) {
+	constructor(props) {
+		super(props);
 		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-			props.setUsers(response.data.items);
-			// {
-			// 	id: 1,
-			// 	followed: false,
-			// 	fullName: 'Vladimir',
-			// 	status: 'I am a boss',
-			// 	location: {city: 'Kiev', country: 'Ukraine'},
-			// 	photoUrl: 'https://yt3.ggpht.com/a-/AAuE7mB-sN6TajR14CcScVbgjsa421akzDrjgknd3g=s900-mo-c-c0xffffffff-rj-k-no'
-			// },
-			// {
-			// 	id: 2,
-			// 	followed: true,
-			// 	fullName: 'Dmitry',
-			// 	status: 'I am boy',
-			// 	location: {city: 'Minsk', country: 'Belarus'},
-			// 	photoUrl: 'https://yt3.ggpht.com/a-/AAuE7mB-sN6TajR14CcScVbgjsa421akzDrjgknd3g=s900-mo-c-c0xffffffff-rj-k-no'
-			// },
-			// {
-			// 	id: 3,
-			// 	followed: false,
-			// 	fullName: 'Joze',
-			// 	status: 'I like music',
-			// 	location: {city: 'Parish', country: 'France'},
-			// 	photoUrl: 'https://yt3.ggpht.com/a-/AAuE7mB-sN6TajR14CcScVbgjsa421akzDrjgknd3g=s900-mo-c-c0xffffffff-rj-k-no'
-			// }
-
+			this.props.setUsers(response.data.items);
 		});
-
-
 	}
 
 
-	return (
-		<div className={styles.container}>
-			{
-				props.users.map((u) => <div key={u.id}>
+	render() {
+		return (
+			<div className={styles.container}>
+				{
+					this.props.users.map((u) => <div key={u.id}>
 					<span>
 						<div>
 							<img src={u.photos.small != null ? u.photos.small : userPhoto} alt="logo"
@@ -51,15 +26,15 @@ let Users = (props) => {
 						<div className={styles.button}>
 							{u.followed
 								? <button onClick={() => {
-									props.unfollow(u.id)
+									this.props.unfollow(u.id)
 								}}>Unfollow</button>
 								: <button onClick={() => {
-									props.follow(u.id)
+									this.props.follow(u.id)
 								}}>Follow</button>
 							}
 						</div>
 					</span>
-					<span>
+						<span>
 						<span>
 							<div>{u.name}</div>
 							<div>{u.status}</div>
@@ -69,10 +44,11 @@ let Users = (props) => {
 							<div>{"u.location.city"}</div>
 						</span>
 					</span>
-				</div>)
-			}
-		</div>
-	)
-};
+					</div>)
+				}
+			</div>
+		)
+	}
+}
 
 export default Users;
